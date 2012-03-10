d6ec138ee7ac412e565364362fe9eff15f2e6a3d
emiko <emiko@opentech.co.jp>
2012-03-10 14:32:48 +0900

    add project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EnjuWs::Application.config.secret_token = '5a2b381d9efa1663fef241053f0ef37b8c3847c021349a8328c0f6b321d7ae62d713da38ba88ecb96d7ba1907db803e10fc38ad27ef09f57e689c082596e58ad'
